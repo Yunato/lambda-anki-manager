61fb459e879308d0b16b0a923c9f5ff98d4a4a21
Yunato <yunato525@gmail.com>
2019-07-29 21:03:31 +0900

    Add insert another table at post_register_func.py

--- lambda_func/post_register_func.py
+++ lambda_func/post_register_func.py
@@ -3,25 +3,33 @@ import boto3
 from boto3.session import Session
 
 def lambda_handler(event, context):
-    region = "ap-northeast-1"
+    region = ""
     session = Session(
         regison_name=region
     )
     dynamodb = session.resource('dynamodb')
 
-    table_name = ''
-    table = dynamodb.Table(table_name)
+    anki_table_name = ''
+    anki_table = dynamodb.Table(anki_table_name)
+    category_table_name = ''
+    category_table = dynamodb.Table(category_table_name)
 
     primary_key = ''
-    content_name = ['']
-    put_response = table.put_item(
+    content_name = []
+    stamp = int(datetime.datetime.now().timestamp())
+    put_card_response = anki_table.put_item(
         Item = {
-            primary_key: int(datetime.datetime.now().timestamp()),
-            # content_name[0]: event[content_name[0]]
+            primary_key: stamp
+            content_name[0]: event[content_name[0]]
         }
     )
 
-    primary_key = ''
+    put_category_response = category_table.put_item(
+        Item = {
+            categories[0]: event[content_name[0]]
+        }
+    )
+    
     scan_response = table.scan()
     scan_response['Items'] = sorted(scan_response['Items'], key=lambda x:x[primary_key], reverse=True)
     return scan_response
