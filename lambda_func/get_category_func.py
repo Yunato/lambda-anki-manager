c15e095cdcc0d9e7bb1f470901d8940c1157ebd5
Yunato <yunato525@gmail.com>
2019-07-29 17:51:22 +0900

    Add lambda_handler() func at get_category_func.py

---
+++ lambda_func/get_category_func.py
@@ -0,0 +1,20 @@
+import boto3
+from boto3.session import Session
+
+def lambda_handler(event, context):
+    region = "ap-northeast-1"
+    session = Session(
+            region_name=region
+    )
+    dynamodb = session.resource('dynamodb')
+
+    table_name = ''
+    table = dynamodb.Table(table_name)
+
+    primary_key = ''
+    secondary_key = ''
+    scan_response = table.scan()
+    scan_response['Items'] = sorted(scan_response['Items'], key=lambda x:x[secondary_key], reverse=True)
+    scan_response['Items'] = sorted(scan_response['Items'], key=lambda x:x[primary_key], reverse=True)
+    return scan_response
+
