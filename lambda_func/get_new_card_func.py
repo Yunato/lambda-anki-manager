4f66f570689cfabf27afd38234084667e12a95fd
Yunato <yunato525@gmail.com>
2019-08-10 21:27:14 +0900

    Create get_new_card_func.py & Add lambda_handler() func

---
+++ lambda_func/get_new_card_func.py
@@ -0,0 +1,26 @@
+import datetime
+import boto3
+from boto3.session import Session
+from boto3.dynamodb.conditions import Key, Attr
+
+def lambda_handler(event, context):
+    region = "ap-northeast-1"
+    session = Session(
+            region_name=region
+    )
+    dynamodb = session.resource('dynamodb')
+
+    table_name = ''
+    table = dynamodb.Table(table_name)
+
+    query_key = ''
+    stamp = int(datetime.datetime.now().replace(hour=0,minute=0,second=0,microsecond=0).timestamp())
+    fe = Key(query_key).gte(stamp)
+    
+    scan_response = table.scan(
+        FilterExpression=fe
+    )
+
+    sort_key = ''
+    scan_response['Items'] = sorted(scan_response['Items'], key=lambda x:x[sort_key], reverse=False)
+    return scan_response
